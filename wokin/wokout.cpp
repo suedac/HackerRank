27513e9f4afe4a7ddcf64d3544f4815baed99793
suedac <zehrasuedacigdem@gmail.com>
2024-03-19 22:11:30 +0100

    EZ

---
+++ wokin/wokout.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,j,y,top;
+    cin>>a>>j>>y;
+    top = a+j+y;
+    cout<<top<<endl;
+    return 0;
+}
