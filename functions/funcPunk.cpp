e287928e59f62da330f833b97c4f46a3a0dcadb5
suedac <zehrasuedacigdem@gmail.com>
2024-03-28 22:48:04 +0100

    sorting stuff

---
+++ functions/funcPunk.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int max_of_four(int a, int b, int c, int d)
+{
+    int biggest;
+    if(a>b){
+        biggest = a;
+    }
+    else {
+        biggest = b;
+    }
+    if(biggest < c){
+        biggest = c;
+    }
+    if(biggest < d){
+        biggest = d;
+    }
+    return biggest;
+}
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
