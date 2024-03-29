0759599cc541aa8bcd03a796b042845b2fee7e32
suedac <zehrasuedacigdem@gmail.com>
2024-03-29 22:14:32 +0100

    pointing stuff.

---
+++ pointer/LOOK!(pointsTheTree).cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+  
+using namespace std;
+void update(int * a, int * b) {
+       int d = *a + *b;
+      *b = abs(*a - *b);
+       *a = d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
