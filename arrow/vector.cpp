3c12e66a8cc1cc16e1494d60fdedc02168480160
suedac <zehrasuedacigdem@gmail.com>
2024-04-01 23:15:09 +0200

    getting variables

---
+++ arrow/vector.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void something(int N, int Q){
+
+}
+int main() {
+int N, Q;
+cin>>N>>Q;
+something(N,Q);
+    
+    return 0;
+}
+
+/*
+2 2
+3 1 5 4
+5 1 2 8 9 3
+0 1
+1 3
+*/
